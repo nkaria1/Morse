7d3259eaa006d6cf65eb5b8179e8daad80ec4543
Niharika Karia <nkaria1@hawk.iit.edu>
2019-07-31 11:39:43 -0400

    intial skeleton
    define the class and methods

---
+++ morse.py
@@ -0,0 +1,26 @@
+import winsound
+import time
+
+class MorseCode:
+    def __init__(self):
+        print("")
+        
+    def dot(self):
+        winsound.Beep(440,200)
+    def dash(self):
+        winsound.Beep(440,600)
+        
+    def translate(self,char):
+        if (char=='A' or char=='a'):
+            mc.dot()
+            mc.dash()
+
+    def encode(text):
+        print (text)
+        
+    def decode(code)
+        print (code)
+        
+        
+if __name__ == "__main__":
+    print ("welcome to morse encoder-decoder")
